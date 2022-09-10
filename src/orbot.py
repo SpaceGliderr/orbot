11dcc4eb62c0e4a90d729bdd6f615f149238ff47
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-09-10 23:45:00 +0800

    Add load_extensions method to orbot.py

--- src/orbot.py
+++ src/orbot.py
@@ -5,6 +5,7 @@ import discord
 from discord.ext import commands
 
 from .config import TOKEN
+from os import walk
 
 
 intents = discord.Intents(
@@ -20,17 +21,19 @@ intents = discord.Intents(
 class Orbot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix=">", case_insensitive=True, intents=intents)
+        self.exts = ["jishaku"]
+        self.cogs_path = "src/cogs"
+        self.cogs_ext_prefix = "src.cogs."
+
 
     def run(self):
         super().run(TOKEN)
 
+
     async def start(self, *args, **kwargs):
         # self.pool = None
 
-        extensions = ["jishaku"]
-
-        for extension in extensions:
-            await self.load_extension(extension)
+        await self.load_extensions()
 
         # while not self.pool:
         #     try:
@@ -39,3 +42,12 @@ class Orbot(commands.Bot):
         #         await asyncio.sleep(1)
 
         await super().start(*args, **kwargs)
+
+
+    async def load_extensions(self):
+        cogs = map(lambda cog: f"{self.cogs_ext_prefix}{cog}", [filename.split('.', 1)[0] for filename in next(walk(self.cogs_path), (None, None, []))[2]])
+
+        extensions = list(cogs) + self.exts
+
+        for extension in extensions:
+            await self.load_extension(extension)
