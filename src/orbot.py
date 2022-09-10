b6299969c1712aac111a4f1cf4d04beac53e2a83
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-09-10 18:38:27 +0800

    Comment out Postgres dependency on bot startup

--- src/orbot.py
+++ src/orbot.py
@@ -19,23 +19,23 @@ intents = discord.Intents(
 
 class Orbot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix="kika ", case_insensitive=True, intents=intents)
+        super().__init__(command_prefix=">", case_insensitive=True, intents=intents)
 
     def run(self):
         super().run(TOKEN)
 
     async def start(self, *args, **kwargs):
-        self.pool = None
+        # self.pool = None
 
         extensions = ["jishaku"]
 
         for extension in extensions:
             await self.load_extension(extension)
 
-        while not self.pool:
-            try:
-                self.pool = await asyncpg.create_pool(user="postgres", host="db")
-            except asyncpg.exceptions.CannotConnectNowError or ConnectionRefusedError:
-                await asyncio.sleep(1)
+        # while not self.pool:
+        #     try:
+        #         self.pool = await asyncpg.create_pool(user="postgres", host="db")
+        #     except asyncpg.exceptions.CannotConnectNowError or ConnectionRefusedError:
+        #         await asyncio.sleep(1)
 
         await super().start(*args, **kwargs)
