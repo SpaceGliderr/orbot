8072ffeba40a6922bd5dc02b9732ab9deb087b72
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-09-15 00:09:21 +0800

    Add helper functions to RolePickerConfig class

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,6 +1,8 @@
-from typing import List
+from typing import Any, List, Optional
 from ruamel.yaml import YAML
-from src.utils.helper import get_from_dict
+from src.utils.helper import dict_has_key, get_from_dict
+
+import discord
 
 
 yaml = YAML(typ="safe")
@@ -8,12 +10,17 @@ yaml = YAML(typ="safe")
 
 class RolePickerConfig:
     def __init__(self) -> None:
-        self.data = yaml.load(open("src/roles.yaml"))
+        with open("src/roles.yaml", "r") as roles_file:
+            self.data = yaml.load(roles_file)
 
 
     @property
     def role_categories(self):
         return get_from_dict(self.data, ["categories", "role_categories"])
+
+
+    def get_data(self):
+        return self.data
 
 
     def get_value(self, path: List[str]):
@@ -37,3 +44,34 @@ class RolePickerConfig:
     def get_role_ids(self, category: str):
         roles = self.get_roles(category)
         return [self.get_role_id(role, category) for role in roles]
+
+
+    def generate_option(self, dic: dict, value: Any, defaults: Optional[Any] = None):
+        option = discord.SelectOption(
+            label=dic["label"],
+            value=value
+        )
+
+        if defaults is not None and option.value in defaults:
+                option.default = True
+
+        if dict_has_key(dic, "emoji"):
+            option.emoji = dic["emoji"]
+
+        if dict_has_key(dic, "description"):
+            option.description = dic["description"]
+         
+        return option
+
+
+    def generate_role_options(self, role_category, defaults: Optional[Any] = None):
+        return [self.generate_option(role, self.get_role_id(role, role_category), defaults) for role in self.get_roles(role_category)]
+
+    
+    def generate_role_category_options(self, defaults: Optional[Any] = None):
+        return [self.generate_option(category, category["name"], defaults) for category in self.role_categories]
+
+    
+    def dump(self, data):
+        with open("src/roles.yaml", "w") as roles_file:
+            yaml.dump(data, roles_file)
