67a0434f2226667dc7fec2b2e7e3eaccd5898c11
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-09-26 06:04:25 +0800

    Add permissions and other decorators to role picker

--- src/cogs/role_picker/role_picker.py
+++ src/cogs/role_picker/role_picker.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 import discord
 import stringcase
-from discord import app_commands
+from discord import Permissions, app_commands
 from discord.ext import commands
 
 from src.cogs.role_picker.ui import (
@@ -17,13 +17,39 @@ from src.utils.config import RolePickerConfig
 from src.utils.helper import dict_has_key
 
 
-
-class RolePicker(commands.Cog):
+class RolePicker(commands.GroupCog, name="role-picker"):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="setup")
+    add_group = app_commands.Group(
+        name="add", description="...", default_permissions=Permissions(manage_roles=True), guild_only=True
+    )
+    edit_group = app_commands.Group(
+        name="edit", description="...", default_permissions=Permissions(manage_roles=True), guild_only=True
+    )
+    delete_group = app_commands.Group(
+        name="delete", description="...", default_permissions=Permissions(manage_roles=True), guild_only=True
+    )
+    remove_group = app_commands.Group(
+        name="remove", description="...", default_permissions=Permissions(manage_roles=True), guild_only=True
+    )
+
+    @app_commands.command(name="overview", description="Shows all categories and roles.")
+    @app_commands.guild_only()
+    @app_commands.default_permissions(use_application_commands=True)
+    async def overview(self, interaction: discord.Interaction):
+        rp_conf = RolePickerConfig()
+
+        embeds = rp_conf.generate_all_embeds()
+        view = RolesOverviewView(embeds=embeds)
+
+        await interaction.response.send_message(embed=embeds[0], view=view)
+
+    @app_commands.command(name="setup", description="Setup the role picker in a text channel.")
+    @app_commands.guild_only()
+    @app_commands.describe(channel="the text channel to setup in")
     @app_commands.default_permissions(manage_roles=True)
+    @app_commands.checks.has_permissions(manage_roles=True)
     async def setup(self, interaction: discord.Interaction, channel: discord.TextChannel):
         await interaction.response.send_message(
             content=f"The role picker has been successfully setup in <#{channel.id}>!"
@@ -32,8 +58,8 @@ class RolePicker(commands.Cog):
             content="Welcome to the bias picker, please select a role category", view=PersistentRolesView()
         )
 
-    @app_commands.command(name="add_role_category")
-    @app_commands.default_permissions(manage_roles=True)
+    @add_group.command(name="category", description="Add a role category.")
+    @app_commands.checks.has_permissions(manage_roles=True)
     async def add_role_category(self, interaction: discord.Interaction):
         """A slash command that allows users with appropriate permissions to add Role Categories.
 
@@ -62,8 +88,9 @@ class RolePicker(commands.Cog):
 
         rp_conf.dump(data)
 
-    @app_commands.command(name="add_role")
-    @app_commands.default_permissions(manage_roles=True)
+    @add_group.command(name="role", description="Add a role to role category(ies).")
+    @app_commands.describe(role="the server role to add", emoji="the role emoji")
+    @app_commands.checks.has_permissions(manage_roles=True)
     async def add_role(self, interaction: discord.Interaction, role: discord.Role, emoji: Optional[str]):
         """A slash command that allows users with appropriate permissions to add Roles to Role Categories.
 
@@ -116,66 +143,39 @@ class RolePicker(commands.Cog):
 
         rp_conf.dump(data)
 
-    @app_commands.command(name="remove_role")
-    @app_commands.default_permissions(manage_roles=True)
-    async def remove_role(self, interaction: discord.Interaction):
+    @edit_group.command(name="category", description="Edit an existing role category.")
+    @app_commands.checks.has_permissions(manage_roles=True)
+    async def edit_role_category(self, interaction: discord.Interaction):
         rp_conf = RolePickerConfig()
 
-        role_category_view = RoleCategoryView(input_type="button", stop_view=True, timeout=90)
+        role_category_view = RoleCategoryView(input_type="button")
 
-        await interaction.response.send_message("Select a role category to remove a role from", view=role_category_view)
+        await interaction.response.send_message("Select role category to edit", view=role_category_view, timeout=90)
         await role_category_view.wait()
         await interaction.delete_original_response()
 
         role_category = role_category_view.values
 
-        roles_view = RolesView(role_category=role_category, timeout=90)
+        idx, category_details = rp_conf.get_role_category(role_category)
 
-        await role_category_view.interaction.response.send_message(
-            f"Select role(s) to remove from {role_category} category", view=roles_view
+        role_category_modal = RoleCategoryModal(
+            title="Edit Role Category", defaults=category_details, custom_id="edit_role_category", timeout=90
         )
-        await roles_view.wait()
-        await role_category_view.interaction.delete_original_response()
+        await role_category_view.interaction.response.send_modal(role_category_modal)
+        await role_category_modal.wait()
 
-        if roles_view.is_confirmed:
-            role_ids_to_remove = [int(role_id) for role_id in roles_view.values]
-            roles = rp_conf.get_roles(role_category)
-
-            data = rp_conf.get_data()
-
-            roles_to_keep = [role for role in roles if role["id"] not in role_ids_to_remove]
-            data[role_category]["roles"] = roles_to_keep
-
-            rp_conf.dump(data)
-        else:
-            await roles_view.interaction.response.send_message(content="No roles were removed")
-
-    @app_commands.command(name="delete_role_categories")
-    @app_commands.default_permissions(manage_roles=True)
-    async def delete_role_categories(self, interaction: discord.Interaction):
-        rp_conf = RolePickerConfig()
-
-        role_category_view = RoleCategoryView(input_type="select", stop_view=True, timeout=90)
-
-        await interaction.response.send_message("Select role category(ies) to delete", view=role_category_view)
-        await role_category_view.wait()
-        await interaction.delete_original_response()
-
-        # TODO: Add double confirmation view
-
-        role_categories = role_category_view.values
+        edited_category = role_category_modal.get_values()
         data = rp_conf.get_data()
 
-        for role_category in role_categories:
-            data["categories"]["role_categories"] = [
-                rc for rc in rp_conf.role_categories if rc["name"] != role_category
-            ]
-            del data[role_category]
+        data["categories"]["role_categories"][idx] = {
+            **data["categories"]["role_categories"][idx],
+            **edited_category,
+        }
 
         rp_conf.dump(data)
 
-    @app_commands.command(name="edit_role")
-    @app_commands.default_permissions(manage_roles=True)
+    @edit_group.command(name="role", description="Edit an existing role.")
+    @app_commands.checks.has_permissions(manage_roles=True)
     async def edit_role(self, interaction: discord.Interaction):
         rp_conf = RolePickerConfig()
 
@@ -211,45 +211,63 @@ class RolePicker(commands.Cog):
         else:
             await roles_view.interaction.response.send_message(content="No roles were edited")
 
-    @app_commands.command(name="edit_role_category")
-    @app_commands.default_permissions(manage_roles=True)
-    async def edit_role_category(self, interaction: discord.Interaction):
+    @delete_group.command(name="categories", description="Delete role category(ies).")
+    @app_commands.checks.has_permissions(manage_roles=True)
+    async def delete_role_categories(self, interaction: discord.Interaction):
         rp_conf = RolePickerConfig()
 
-        role_category_view = RoleCategoryView(input_type="button")
+        role_category_view = RoleCategoryView(input_type="select", stop_view=True, timeout=90)
 
-        await interaction.response.send_message("Select role category to edit", view=role_category_view, timeout=90)
+        await interaction.response.send_message("Select role category(ies) to delete", view=role_category_view)
+        await role_category_view.wait()
+        await interaction.delete_original_response()
+
+        # TODO: Add double confirmation view
+
+        role_categories = role_category_view.values
+        data = rp_conf.get_data()
+
+        for role_category in role_categories:
+            data["categories"]["role_categories"] = [
+                rc for rc in rp_conf.role_categories if rc["name"] != role_category
+            ]
+            del data[role_category]
+
+        rp_conf.dump(data)
+
+    @remove_group.command(name="role", description="Remove a role from a role category.")
+    @app_commands.checks.has_permissions(manage_roles=True)
+    async def remove_role(self, interaction: discord.Interaction):
+        rp_conf = RolePickerConfig()
+
+        role_category_view = RoleCategoryView(input_type="button", stop_view=True, timeout=90)
+
+        await interaction.response.send_message("Select a role category to remove a role from", view=role_category_view)
         await role_category_view.wait()
         await interaction.delete_original_response()
 
         role_category = role_category_view.values
 
-        idx, category_details = rp_conf.get_role_category(role_category)
+        roles_view = RolesView(role_category=role_category, timeout=90)
 
-        role_category_modal = RoleCategoryModal(
-            title="Edit Role Category", defaults=category_details, custom_id="edit_role_category", timeout=90
+        await role_category_view.interaction.response.send_message(
+            f"Select role(s) to remove from {role_category} category", view=roles_view
         )
-        await role_category_view.interaction.response.send_modal(role_category_modal)
-        await role_category_modal.wait()
+        await roles_view.wait()
+        await role_category_view.interaction.delete_original_response()
 
-        edited_category = role_category_modal.get_values()
-        data = rp_conf.get_data()
+        if roles_view.is_confirmed:
+            role_ids_to_remove = [int(role_id) for role_id in roles_view.values]
+            roles = rp_conf.get_roles(role_category)
 
-        data["categories"]["role_categories"][idx] = {
-            **data["categories"]["role_categories"][idx],
-            **edited_category,
-        }
+            data = rp_conf.get_data()
 
-        rp_conf.dump(data)
+            roles_to_keep = [role for role in roles if role["id"] not in role_ids_to_remove]
+            data[role_category]["roles"] = roles_to_keep
 
-    @app_commands.command(name="overview")
-    async def roles_overview(self, interaction: discord.Interaction):
-        rp_conf = RolePickerConfig()
-
-        embeds = rp_conf.generate_all_embeds()
-        view = RolesOverviewView(embeds=embeds)
-
-        await interaction.response.send_message(embed=embeds[0], view=view)
+            rp_conf.dump(data)
+        else:
+            await roles_view.interaction.response.send_message(content="No roles were removed")
 
 
 async def setup(bot):
