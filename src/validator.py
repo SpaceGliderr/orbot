f7f7c7fb930cdebc80a35a18b1c62bcdde1b0bbd
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-09-26 04:13:55 +0800

    Remove old files

--- src/validator.py
+++
@@ -1,5 +0,0 @@
-from schema import Schema
-
-preset_structure = Schema([{"name": str, "type": str, "text": str, "dropdown_options": []}])
-
-overall_structure = Schema({"presets": preset_structure, "role_categories": list, "messages": list})
