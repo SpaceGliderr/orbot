0604f85c9dfbc7ccef672d8db540ffa03aa1a849
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-10-12 16:06:26 +0800

    Add fansite and helper py to listen to realtime tweets and post it to Discord

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -1,5 +1,10 @@
 from functools import reduce
 
+import io
+from typing import List
+import aiohttp
+import discord
+
 
 def get_from_dict(dic, map_list):
     """Iterate nested dictionary"""
@@ -11,3 +16,16 @@ def get_from_dict(dic, map_list):
 
 def dict_has_key(dic, key):
     return key in dic.keys()
+
+
+async def download_files(urls: List[str]):
+    return [await download_file(url, idx) for idx, url in enumerate(urls)]
+
+
+async def download_file(url: str, name: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status != 200:
+                raise Exception("Cannot download file")
+            data = io.BytesIO(await resp.read())
+            return discord.File(data, f'{name}.png')
