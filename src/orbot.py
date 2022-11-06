bd36ad3017b768a545941ef276f5deafc4b66470
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2022-11-06 17:41:29 +0800

    Remove unused imports in orbot

--- src/orbot.py
+++ src/orbot.py
@@ -5,7 +5,6 @@ import discord
 from discord.ext import commands
 
 from src.cogs.content_poster.ui.views.persistent import PersistentTweetView
-
 from src.cogs.role_picker.ui import PersistentRolePickerView
 from src.modules.twitter.feed import TwitterFeed
 from src.utils.config import ContentPosterConfig
