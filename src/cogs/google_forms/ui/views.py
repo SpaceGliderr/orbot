d4729d4d70a4498119d0a3f7c2052f8af3033de8
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2023-01-05 02:31:46 +0800

    Edit views.py

--- src/cogs/google_forms/ui/views.py
+++ src/cogs/google_forms/ui/views.py
@@ -23,22 +23,25 @@ class AuthenticationLinkView(View):
     def __init__(self, auth_url: str, timeout: Optional[float] = None):
         super().__init__(timeout=timeout)
         self.interaction = None
-        self.add_item(discord.ui.Button(style=discord.ButtonStyle.link, url=auth_url, label="Authenticate with Google"))
         self.auth_code = None
         self.is_cancelled = False
 
-    @discord.ui.button(label="Enter Authentication Code", style=discord.ButtonStyle.primary)
-    async def enter_code(self, interaction: discord.Interaction, *_):
-        enter_code_modal = AuthenticationCodeModal(title="Enter Authentication Code", timeout=20)
-        await interaction.response.send_modal(enter_code_modal)
-        timeout = await enter_code_modal.wait()
+        self.add_item(
+            discord.ui.Button(style=discord.ButtonStyle.link, url=auth_url, label="Authenticate with Google", row=0)
+        )
 
+    @discord.ui.button(label="Enter Authentication Code", style=discord.ButtonStyle.primary, row=1)
+    async def enter_code(self, interaction: discord.Interaction, *_):
+        auth_code_modal = AuthenticationCodeModal(title="Enter Authentication Code", timeout=20)
+        await interaction.response.send_modal(auth_code_modal)
+
+        timeout = await auth_code_modal.wait()
         if not timeout:
-            self.auth_code = enter_code_modal.get_values()["auth_code"]
+            self.auth_code = auth_code_modal.get_values()["auth_code"]
             self.interaction = interaction
             self.stop()
 
-    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red, emoji="✖️")
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red, emoji="✖️", row=1)
     async def cancel(self, interaction: discord.Interaction, *_):
         await interaction.response.defer()
         self.is_cancelled = True
