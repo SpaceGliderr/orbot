94db1fded23203275520e08a433f184d8e719b71
Nilch <46506221+SpaceGliderr@users.noreply.github.com>
2023-01-05 02:44:02 +0800

    Refactor service.py

--- src/modules/google_forms/service.py
+++ src/modules/google_forms/service.py
@@ -1,7 +1,4 @@
-import asyncio
-import pprint
-from http.client import HTTPException
-from typing import Optional
+from typing import Literal, Optional
 
 import discord
 from apiclient import discovery
@@ -14,7 +11,7 @@ from src.utils.helper import get_from_dict
 class GoogleFormsService:
     def __init__(self, credentials) -> None:
         self.credentials = credentials
-        self.service = discovery.build("forms", "v1", credentials=credentials)
+        self.forms_service = discovery.build("forms", "v1", credentials=credentials)
         self.sheets_service = discovery.build("sheets", "v4", credentials=credentials)
 
     @classmethod
@@ -25,105 +22,88 @@ class GoogleFormsService:
     def init_service_acc(cls):
         return cls(GoogleCredentials.get_service_acc_cred())
 
-    def get_watch(
-        self, form_id: str, event_type: str, topic_name: Optional[str] = None, watch_id: Optional[str] = None
-    ):
-        watch_list = self.service.forms().watches().list(formId=form_id).execute()
-        print("WATCH LIST >>> ", watch_list)
-
-        if watch_list == {}:
+    def get_form_details(self, form_id: str):
+        try:
+            return self.forms_service.forms().get(formId=form_id).execute()
+        except:
             return None
 
-        def is_valid(watch: dict):
-            if event_type and not watch["eventType"] == event_type:
-                return False
-            if topic_name and not get_from_dict(watch, ["target", "topic", "topicName"]) == topic_name:
-                return False
-            if watch_id and not watch["id"] == watch_id:
-                return False
-            return True
+    def get_form_watches(self, form_id: str):
+        form_watches = self.forms_service.forms().watches().list(formId=form_id).execute()
+        return form_watches if form_watches != {} else None
 
-        result = next(
-            (watch for watch in watch_list["watches"] if is_valid(watch)),
-            None,
-        )
-        return result
-
-    async def create_watch_list(
-        self, form_id: str, event_type: str, interaction: discord.Interaction, topic_name: Optional[str] = None
+    def filter_form_watch(
+        self,
+        form_id: str,
+        watch_id: Optional[str] = None,
+        event_type: Optional[Literal["RESPONSES", "SCHEMA"]] = None,
+        topic_name: Optional[str] = None,
     ):
-        result = None
-        topic_name = topic_name if topic_name else "projects/loonacord/topics/form-responses"
-        print("CREDENTIALS >>> ", self.credentials)
+        form_watches = self.get_form_watches(form_id=form_id)
+        if form_watches:
+            return next(
+                list(
+                    filter(
+                        lambda watch: False
+                        if watch_id and not watch["id"] == watch_id
+                        else False
+                        if event_type and not watch["event_type"] == event_type
+                        else False
+                        if topic_name and not get_from_dict(watch, ["target", "topic", "topicName"]) == topic_name
+                        else True,
+                        form_watches["watches"],
+                    )
+                ),
+                None,
+            )
+        return None
+
+    def create_form_watch(self, form_id: str, event_type: Literal["RESPONSES", "SCHEMA"], topic_name: str):
+        form_watch = None
+
         try:
-            result = (
-                self.service.forms()
+            form_watch = (
+                self.forms_service.forms()
                 .watches()
                 .create(
                     formId=form_id,
                     body={
                         "watch": {
-                            "target": {"topic": {"topicName": "projects/loonacord/topics/form-responses"}},
+                            "target": {"topic": {"topicName": topic_name}},
                             "eventType": event_type,
                         }
                     },
                 )
                 .execute()
             )
-            print("CREATE WATCH RESULT >>> ", result)
         except HttpError as err:
-            print("ERROR >>> ", err)
-            if err.status_code == 400:  # Means the particular form watch exists already
-                result = self.get_watch(
-                    form_id=form_id, event_type=event_type, topic_name="projects/loonacord/topics/form-responses"
-                )
-                print("GET WATCH RESULT >>> ", result)
+            if err.status_code == 400:
+                form_watch = self.filter_form_watch(form_id=form_id, event_type=event_type, topic_name=topic_name)
+        except Exception as err:
+            form_watch = None
 
-                # loop = asyncio.get_event_loop()
-                # asyncio.set_event_loop(loop)
+        return form_watch
 
-                await interaction.followup.send(content="ALREADY FOUND THIS SHIT", ephemeral=True)
-
-                # loop.run_until_complete(interaction.response.send_message(content="ALREADY FOUND THIS SHIT", ephemeral=True))
-                # asyncio.run(interaction.response.send_message(content="ALREADY FOUND THIS SHIT", ephemeral=True))
+    def delete_form_watch(self, form_id: str, watch_id: str):
+        try:
+            return self.forms_service.forms().watches().delete(formId=form_id, watchId=watch_id).execute()
         except:
-            result = None
+            return None
 
-        print("WATCH RESULT >>> ", result)
-        return result
+    def get_sheet(self, sheet_id: str, cell_range: str = "Form Responses 1"):
+        try:
+            return self.sheets_service.spreadsheets().values().get(spreadsheetId=sheet_id, range=cell_range).execute()
+        except:
+            return None
 
-    def get_latest_response(self, form_id: str, sheet_id: str):
-        result = self.get_sheet(sheet_id)
+    def get_latest_form_response(self, form_id: str, sheet_id: str):
+        sheet = self.get_sheet(sheet_id)
+        sheet_responses = get_from_dict(sheet, ["values"])
 
-        if result:
-            questions = result["values"][0]
-            answers = result["values"][-1]
+        if sheet_responses and len(sheet_responses) > 1:
+            questions = sheet_responses[0]
+            answers = sheet_responses[-1]
             return [{str(question): answer} for question, answer in zip(questions, answers)]
 
-        print("RESULT TITLE OF QUESTIONS >>> ", result["values"][0])
-        print("LATEST RESPONSE >>> ", result["values"][-1])
-        # pprint(result)
-
-        result = self.service.forms().responses().list(formId=form_id).execute()
-        print("RESULTS >>> ", result)
-        if len(result) > 0:
-            return result["responses"][0]
-        return None
-
-    def get_form_details(self, form_id: str):
-        try:
-            return self.service.forms().get(formId=form_id).execute()
-        except:
-            return None
-
-    def get_sheet(self, sheet_id: str):
-        print("SHEET ID >>> ", sheet_id)
-        try:
-            return (
-                self.sheets_service.spreadsheets()
-                .values()
-                .get(spreadsheetId=sheet_id, range="Form Responses 1")
-                .execute()
-            )
-        except:
-            return None
+        form_responses = self.forms_service.forms().responses().list(formId=form_id).execute()
+        return form_responses["responses"][0] if len(form_responses) > 0 else None
